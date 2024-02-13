02b2cefe34888efe6613d8e1d1370d4b21db21e3
Drew Budwin <drew@budw.in>
2024-02-13 09:20:32 -0600

    fix(baz): long enough header

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,20 +1,21 @@
-// module.exports = {extends: ['@commitlint/config-conventional']}
+// module.exports = {extends: ["@commitlint/config-conventional"]}
 
-import {RuleConfigSeverity} from '@commitlint/types';
-import type { UserConfig } from '@commitlint/types';
+import { RuleConfigSeverity } from "@commitlint/types";
+import type { UserConfig } from "@commitlint/types";
 
 const Configuration: UserConfig = {
   /*
    * Resolve and load @commitlint/config-conventional from node_modules.
    * Referenced packages must be installed
    */
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
 
   /*
    * Any rules defined here will override rules from @commitlint/config-conventional
    */
   rules: {
-    'scope-empty': [RuleConfigSeverity.Disabled, 'always'],
+    "header-min-length": [RuleConfigSeverity.Error, "always", 20],
+    "scope-empty": [RuleConfigSeverity.Error, "never"],
   },
 };
 
